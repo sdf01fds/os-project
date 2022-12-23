49da9398a1068264394d74bc1efcc089e9f84ef1
sdf01fds <sdf0106@protonmail.com>
2022-12-23 15:20:08 +0500

    handlers added

--- wallet.go
+++ wallet.go
@@ -4,7 +4,7 @@ import "time"
 
 type Wallet struct {
 	Id         int       `json:"id" db:"id"`
-	UserId     int       `json:"user_id" db:"sender_id"`
+	UserId     int       `json:"user_id" db:"user_id"`
 	CreatedAt  time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at" db:"updated_at"`
 	PrivateKey string    `json:"private_key" db:"private_key"`
